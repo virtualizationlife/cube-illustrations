07267f0bea1df454077d51279a5df8f4c20847ba
Pavel Kuzmin <mpakunderscore@gmail.com>
2026-08-24 14:21:26 -0700

    feat: connect scene group switcher and split tokens

--- src/app/IllustrationsPage.tsx
+++ src/app/IllustrationsPage.tsx
@@ -3,16 +3,34 @@ import type { JSX } from 'react'
 import { SCENE_CATALOG } from '@app/sceneCatalog'
 import { SceneRenderHost } from '@runtime/rendering/SceneRenderHost'
 
+import { ALL_SCENES_GROUP, SceneGalleryProvider, useSceneGallery } from './SceneGalleryContext'
+import { SceneGroupSwitcher } from './SceneGroupSwitcher'
+
+const IllustrationsContent = (): JSX.Element => {
+    const { activeGroup } = useSceneGallery()
+    const visibleScenes =
+        activeGroup === ALL_SCENES_GROUP
+            ? SCENE_CATALOG
+            : SCENE_CATALOG.filter(({ primaryCategory }) => primaryCategory === activeGroup)
+
+    return (
+        <SceneRenderHost>
+            <div className='cube_illustrations__page'>
+                <SceneGroupSwitcher />
+                {visibleScenes.map(({ id, title, tags, component: SceneComponent }) => (
+                    <div className='cube_illustrations__labeled_scene' key={id}>
+                        <SceneComponent />
+                        <p className='cube_illustrations__scene_label'>{title}</p>
+                        <p className='cube_illustrations__scene_tags'>{tags.join(', ')}</p>
+                    </div>
+                ))}
+            </div>
+        </SceneRenderHost>
+    )
+}
+
 export const IllustrationsPage = (): JSX.Element => (
-    <SceneRenderHost>
-        <div className='cube_illustrations__page'>
-            {SCENE_CATALOG.map(({ id, title, tags, component: SceneComponent }) => (
-                <div className='cube_illustrations__labeled_scene' key={id}>
-                    <SceneComponent />
-                    <p className='cube_illustrations__scene_label'>{title}</p>
-                    <p className='cube_illustrations__scene_tags'>{tags.join(', ')}</p>
-                </div>
-            ))}
-        </div>
-    </SceneRenderHost>
+    <SceneGalleryProvider>
+        <IllustrationsContent />
+    </SceneGalleryProvider>
 )
